5a0b2e84d6b745a646881aca8eb4233b37e134a2
신재훈 <shinjaehun@sinjaehun-ui-MacBook-Pro.local>
2025-02-02 05:53:21 +0900

    초기 프로젝트 환경 설정 완료

---
+++ backend/src/main/java/com/gyerangnarang/gyerangnarang_backend/GyerangnarangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gyerangnarang.gyerangnarang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyerangnarangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyerangnarangBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gyerangnarang/gyerangnarang_backend/GyerangnarangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyerangnarang.gyerangnarang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyerangnarangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
